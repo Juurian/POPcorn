2089e98a4b19401093b36567a2011076130c3cbc
BradyWallaceACE <bwallace2@academic.rrc.ca>
2023-11-30 14:54:46 -0600

    Added dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -84,6 +84,8 @@ dependencies {
     implementation("com.google.firebase:firebase-database:20.3.0")
     implementation("com.google.android.gms:play-services-auth:20.7.0")
     implementation("androidx.lifecycle:lifecycle-runtime-compose:2.6.2")
+    implementation("com.google.firebase:firebase-auth-ktx:22.3.0")
+    implementation("androidx.window:window:1.2.0")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
@@ -91,4 +93,5 @@ dependencies {
     androidTestImplementation("androidx.compose.ui:ui-test-junit4")
     debugImplementation("androidx.compose.ui:ui-tooling")
     debugImplementation("androidx.compose.ui:ui-test-manifest")
+    implementation ("androidx.compose.material3:material3-window-size-class:1.1.0")
 }
